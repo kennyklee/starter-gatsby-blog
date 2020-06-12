c92b1c9461d38eadfae3337c60228e8b794c3d7d
Kenny Lee <kennyklee@gmail.com>
2020-06-11 20:41:04 -0700

    update posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -5,10 +5,41 @@ import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
 // ...GatsbyImageSharpFluid
 const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
   return (
     <Layout>
       <Hero />
+      <Posts posts={posts} title="all posts"></Posts>
     </Layout>
   )
 }
 export default PostsPage
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          date(formatString: "MMMM Do, YYYY")
+          author
+          slug
+          readTime
+          category
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
